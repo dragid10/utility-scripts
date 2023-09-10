961e1c68ad99001755604b7dfa58ad42571f7ba8
Alex Oladele <dragid10@gmail.com>
2023-09-10 12:28:09 -0400

    🔨 Actually finish out label printer setup script

--- server/cups-server/label-printer-setup.sh
+++ server/cups-server/label-printer-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "Downloading driver"
 curl https://cdn.shopify.com/s/files/1/0595/9739/0033/files/polono_pl60_label_printer_linux_driver_v1.2.0.tar.gz -o polono_driver.tar.gz
 
 mkdir polono-driver
@@ -8,4 +9,6 @@ cd polono-driver
 cd $(ls -d */|head -n 1)
 sed -i 's/$INSTALL -s/$INSTALL/g' install
 sed -i '/if \[ -z \$RPMBUILD \]/,$d' install
-#sed '/if [ -z $RPMBUILD ]/Q' install
+
+echo "Running Driver install script"
+bash install
